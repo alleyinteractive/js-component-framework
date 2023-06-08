e342c567d24be237a610efff80f1b6eae518e1ae
Ryan Domingue <ryan@alley.co>
2023-06-08 10:27:06 -0700

    Removes the domContentLoaded export

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import componentProvider from './componentProvider';
 import componentLoader from './componentLoader';
-import domContentLoaded from './domContentLoaded';
 
 export {
   componentProvider,
   componentLoader,
-  domContentLoaded,
 };
