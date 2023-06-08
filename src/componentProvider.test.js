f558537a4a138eddc203f2f1d2e3c534202e13d9
Ryan Domingue <ryan@alley.co>
2023-06-07 18:02:01 -0700

    Configures Jest and adds initial tests

---
+++ src/componentProvider.test.js
@@ -0,0 +1,113 @@
+/* eslint-disable max-len */
+import componentProvider from './componentProvider';
+
+document.body.innerHTML = `
+  <div id="test-one" data-component="test-one">
+    <button class="cool-button">Cool Button!</button>
+    <ul>
+      <li>First List Item</li>
+      <li>Second List Item</li>
+      <li>Third List Item</li>
+    </ul>
+  </div>
+  <div id="test-two">
+    <div data-component="test-two">
+      <button class="cool-button">Cool Button!</button>
+      <ul>
+        <li>First List Item</li>
+        <li>Second List Item</li>
+        <li>Third List Item</li>
+      </ul>
+    </div>
+    <div data-component="test-two">
+      <button class="cool-button">Cool Button!</button>
+      <ul>
+        <li>First List Item</li>
+        <li>Second List Item</li>
+        <li>Third List Item</li>
+      </ul>
+    </div>
+  </div>
+`;
+
+afterEach(() => jest.clearAllMocks());
+
+const baseConfig = {
+  component: jest.fn(),
+  querySelector: {
+    button: 'button',
+    nothing: '.nonexistent-class',
+  },
+  querySelectorAll: {
+    listItems: 'li',
+  },
+  options: { testing: true },
+};
+
+// Compile expected args for single instance.
+const element = document.querySelector('#test-one');
+const singleExpected = {
+  element,
+  children: {
+    button: element.querySelector('button'),
+    listItems: Array.from(element.querySelectorAll('li')),
+    nothing: null,
+  },
+  options: { testing: true },
+};
+
+// Compile expected args for multiple instances.
+const wrapper = document.querySelector('#test-two');
+const elements = wrapper.querySelectorAll(':scope > div');
+const testTwoExpected = Array.from(elements)
+  .map((div) => ({
+    element: div,
+    children: {
+      button: div.querySelector('button'),
+      listItems: Array.from(div.querySelectorAll('li')),
+      nothing: null,
+    },
+    options: { testing: true },
+  }));
+
+test('Provides expected arguments for single instance', () => {
+  const config = { ...baseConfig, name: 'test-one' };
+
+  componentProvider(config);
+  expect(config.component).toHaveBeenCalledTimes(1);
+  expect(config.component).toHaveBeenCalledWith(singleExpected);
+});
+
+test('Provides expected arguments for multiple instances', () => {
+  const config = { ...baseConfig, name: 'test-two' };
+
+  componentProvider(config);
+  expect(config.component).toHaveBeenCalledTimes(2);
+
+  expect(config.component).toHaveBeenNthCalledWith(1, testTwoExpected[0]);
+  expect(config.component).toHaveBeenNthCalledWith(2, testTwoExpected[1]);
+});
+
+test('Returns the expected function for single instance when `load:false`', () => {
+  const config = { ...baseConfig, name: 'test-one', load: false };
+
+  const providerFunction = componentProvider(config);
+  expect(config.component).toHaveBeenCalledTimes(0);
+
+  providerFunction();
+  expect(config.component).toHaveBeenCalledTimes(1);
+  expect(config.component).toHaveBeenCalledWith(singleExpected);
+});
+
+test('Returns the expected function for multiple instances when `load:false`', () => {
+  const config = { ...baseConfig, name: 'test-two', load: false };
+
+  const providerFunction = componentProvider(config);
+  expect(config.component).toHaveBeenCalledTimes(0);
+
+  providerFunction();
+  expect(config.component).toHaveBeenCalledTimes(2);
+
+  expect(config.component).toHaveBeenNthCalledWith(1, testTwoExpected[0]);
+  expect(config.component).toHaveBeenNthCalledWith(2, testTwoExpected[1]);
+});
